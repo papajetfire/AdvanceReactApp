0fde8932efc612589a91f2e79a611ec00546a097
Soumya Sheel <soumyasheel2011@gmail.com>
2021-08-11 09:14:11 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import UseStateErrorEx from './Tutorial/UseStateErrorEx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1> This is an advance app</h1>
+      <UseStateErrorEx />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Tutorial/UseStateErrorEx.js
+++ src/Tutorial/UseStateErrorEx.js
@@ -1,14 +1,21 @@
 import React,{useState} from 'react'
-
-let title="random text";
+let title="Some random text"; 
+let newTitle="Not so random"
+function UseStateErrorEx(){
+const[text,setText]=useState(()=> title);
 function ClickHandler() {
-    title="Not so random";
-    console.log(title)
-}
-function UseStateErrorEx() {
+    setText(prevText => {
+        if (prevText===title){
+            prevText=newTitle
+        }
+        else{
+            prevText=title
+        }
+        return prevText
+})}
     return (
         <div>
-            <h1>{title}</h1>
+            <h1>{text}</h1>
             <button type ='button' onClick={ClickHandler}> Click </button>
             
             
