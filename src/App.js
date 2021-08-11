383e40c3e515f31e6d56343e848790e80946dcea
Soumya Sheel <soumyasheel2011@gmail.com>
2021-08-12 02:02:16 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 //import UseStateErrorEx from './Tutorial/UseStateErrorEx'
-import UseStateArray from './Tutorial/UseStateArray'
+//import UseStateArray from './Tutorial/UseStateArray'
 //import UseStateObject from './UseStateObject'
+import UseStateCounter from './Tutorial/UseStateCounter'
 
 function App() {
   return (
     <div className='container'>
       <h1> This is an advance app</h1>
-      <UseStateArray />
+      
+      <UseStateCounter/>
       
     </div>
   )
